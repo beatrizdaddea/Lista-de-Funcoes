b0862f2bcd78f33a834616c4e894e18ce89014aa
Beah25 <79612466+Beah25@users.noreply.github.com>
2021-11-23 16:56:13 -0300

    ex de funções

---
+++ ex_def1.py
@@ -0,0 +1,6 @@
+""" Crie uma função que exibe ua saudação com os parametros saudação nome"""
+
+def saudacao(saudacao, nome):
+    print(f"{saudacao} {nome}")
+
+saudacao ('Oi','Bia')
---
+++ ex_def2.py
@@ -0,0 +1,6 @@
+""" crie uma função que receba 2 numeros. primiero: inteiro. segundo é percentual. retorne o valor do primiero somado co o auento percentual"""
+
+def aumento(valor,percentual):
+    print(valor +(valor * percentual / 100)) 
+
+aumento(50,200)
